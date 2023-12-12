18a757be89959bf901f5cd1c427eb4fa3d1fcd5e
wym1987 <151411252+wym1987@users.noreply.github.com>
2023-12-12 14:52:40 +0800

    Add files via upload

--- prebuild.sh
+++ prebuild.sh
@@ -6,4 +6,10 @@ dpkg-reconfigure locales
 
 sudo apt-get update && DEBIAN_FRONTEND=noninteractive sudo apt install -y tzdata && sudo apt-get install -y git curl wget apt-utils rsync
 
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+sudo chmod 644 /usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod 644 /etc/apt/sources.list.d/github-cli.list
+
 sudo apt-get update && sudo apt-get install gh -y
